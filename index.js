f1cd38a831d03b4969c95606df52ac0126727054
soharab007 <soharabhossain943@gmail.com>
2023-02-25 08:40:25 +0600

    orders section allmost done

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//
+
+// module 67.8 vvi
+
 // database
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8lhtxek.mongodb.net/?retryWrites=true&w=majority`;
@@ -38,9 +42,27 @@ async function run() {
       res.send(service);
 
       // orders api
+
+      app.get("/orders", async (req, res) => {
+        let query = {};
+        if (req.query.email) {
+          query = { email: req.query.email };
+        }
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      });
+
       app.post("/orders", async (req, res) => {
         const order = req.body;
         const result = await orderCollection.insertOne(order);
+        res.send(result);
+      });
+
+      app.delete("/orders/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await orderCollection.deleteOne(query);
         res.send(result);
       });
     });
