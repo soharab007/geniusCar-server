981512886c3fa722f37a3258b568f259552a6fe6
soharab007 <soharabhossain943@gmail.com>
2023-03-03 17:07:33 +0600

    jwt

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ async function run() {
     app.post("/jwt", (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "5",
+        expiresIn: "1d",
       });
       res.send({ token });
     });
